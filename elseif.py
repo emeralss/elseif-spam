05955525ffb1612fb7347fc8db69bf3fced6ca5e
emeralss <emeralsblue11@gmail.com>
2024-05-31 22:41:10 -0400

    Add files via upload

---
+++ elseif.py
@@ -0,0 +1,21 @@
+iterations = 100000
+
+file = open("thethingy.lua", "w")
+
+string = 'local x = 100\nif x == 1 then\n    print("x is odd")\n'
+for i in range(2,iterations,1):
+    if i%2 == 0:
+        string += 'elseif x == '+str(i)+' then\n    print("x is even")\n'
+    else:
+        string += 'elseif x == '+str(i)+' then\n    print("x is odd")\n'	
+    print(i)	
+
+if iterations%2 == 0:
+    string += 'elseif x == '+str(i)+' then\n    print("x is even")\nend'
+else:
+    string += 'elseif x == '+str(i)+' then\n    print("x is odd")\nend'
+
+file.write(string)
+file.close()
+
+print("the program has finished")
