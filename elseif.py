e173ff1c299637736de8b51a175db6df151f44ce
emerald <emeralsblue11@gmail.com>
2024-06-01 12:46:59 -0400

    made a major optimization to the code

--- elseif.py
+++ elseif.py
@@ -1,6 +1,12 @@
-iterations = 100000
+iterations = 10000000
 
 file = open("thethingy.lua", "w")
+
+file.write("")
+
+file.close()
+
+file = open("thethingy.lua", "a")
 
 string = 'if x == 1 then\n    print("x is odd")\n'
 
@@ -8,8 +14,12 @@ for i in range(2,iterations,1):
     if i%2 == 0:
         string += 'elseif x == '+str(i)+' then\n    print("x is even")\n'
     else:
-        string += 'elseif x == '+str(i)+' then\n    print("x is odd")\n'	
-    print(i)	
+        string += 'elseif x == '+str(i)+' then\n    print("x is odd")\n'
+
+    if i%1000 == 0:
+        print(i)
+        file.write(string)
+        string = ""
 
 if (iterations)%2 == 0:
     string += 'elseif x == '+str(iterations)+' then\n    print("x is even")\nend'
@@ -18,5 +28,7 @@ else:
 
 file.write(string)
 file.close()
+
+string = ""
 
 print("the program has finished")
